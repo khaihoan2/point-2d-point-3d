39bc35c545023c886220ac2cfc9d12df15a03b1a
khaihoan <ngonguyenkhaihoan@gmail.com>
2021-08-16 09:18:40 +0700

    bt

---
+++ src/tPoint3D/tPoint2D.java
@@ -0,0 +1,9 @@
+package tPoint3D;
+
+public class tPoint2D {
+        public static void main(String[] args) {
+            Point2D point2D = new Point2D(2, 3);
+            System.out.println(point2D);
+        }
+
+}
---
+++ src/tPoint3D/tPoint3D.java
@@ -0,0 +1,8 @@
+package tPoint3D;
+
+public class tPoint3D {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D(1,2,3);
+        System.out.println(point3D);
+    }
+}
